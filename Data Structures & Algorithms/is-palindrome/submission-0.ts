ce908d649574a80e676737a7fec3c28fda653a19
bharathganji <59326887+bharathganji@users.noreply.github.com>
2026-08-26 16:05:41 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,28 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  isPalindrome(s: string): boolean {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left <= right) {
+      while (left < right && !/[a-zA-Z0-9]/.test(s[left])) {
+        left++;
+      }
+
+      while (left < right && !/[a-zA-Z0-9]/.test(s[right])) {
+        right--;
+      }
+
+      if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+        return false;
+      }
+
+      left++;
+      right--;
+    }
+    return true;
+  }
+}
