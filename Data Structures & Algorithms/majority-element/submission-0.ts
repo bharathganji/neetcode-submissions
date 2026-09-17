bd1a4051d7df4d422bc383712d0a497e31b49bb4
bharathganji <59326887+bharathganji@users.noreply.github.com>
2026-09-17 12:03:15 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  majorityElement(nums: number[]): number {
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+      map.set(nums[i], (map.get(nums[i]) ?? 0) + 1);
+
+      if (map.get(nums[i]) > nums.length / 2) {
+        return nums[i];
+      }
+    }
+
+    return -1;
+  }
+}
